7c0eb3f729c9d9bfaca2bfeef7868acea5084eb4
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-23 21:58:38 +0600

    run server complete

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('SitPad server is runing');
+});
+
+app.listen(port, () => {
+	console.log(`SitPad server is running on ${port}`);
+});
