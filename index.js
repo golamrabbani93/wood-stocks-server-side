6008433b9abd9d79364fbb4fcfdc48ec87ac6b2f
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-24 12:29:03 +0600

    get all products categories

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const {MongoClient, ServerApiVersion} = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 require('dotenv').config();
-const {MongoClient, ServerApiVersion} = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qh4qhby.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 	serverApi: ServerApiVersion.v1,
 });
-
+async function run() {
+	try {
+		const categories = client.db('sitpad').collection('category');
+		app.get('/categories', async (req, res) => {
+			const query = {};
+			const cursor = categories.find(query);
+			const result = await cursor.toArray();
+			console.log('🚀🚀: run -> result', result);
+			res.send(result);
+		});
+	} finally {
+	}
+}
+run().catch((err) => console.log(err));
 app.get('/', (req, res) => {
 	res.send('SitPad server is runing');
 });
