35f846d7a99c0efc96786754983972929f43b5f3
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-24 11:19:04 +0600

    connect mongodb database to server

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+require('dotenv').config();
+const {MongoClient, ServerApiVersion} = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qh4qhby.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	serverApi: ServerApiVersion.v1,
+});
 
 app.get('/', (req, res) => {
 	res.send('SitPad server is runing');
