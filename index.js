9032206eb35d8e75591f4034ef44348f4abf8fda
golamrabbani93 <golamrabbani9373@gmail.com>
2022-11-24 13:17:10 +0600

    get specific category products

--- index.js
+++ index.js
@@ -16,11 +16,20 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		const categories = client.db('sitpad').collection('category');
+		const products = client.db('sitpad').collection('products');
+		//*products category
 		app.get('/categories', async (req, res) => {
 			const query = {};
 			const cursor = categories.find(query);
 			const result = await cursor.toArray();
-			console.log('🚀🚀: run -> result', result);
+			res.send(result);
+		});
+		//*load category product
+		app.get('/products/:category_id', async (req, res) => {
+			const id = parseInt(req.params.category_id);
+			const query = {category_id: id};
+			const cursor = products.find(query);
+			const result = await cursor.toArray();
 			res.send(result);
 		});
 	} finally {
